0446e67524e2c8f61ac8159c8c98bfe741df522e
neynt <hyriodula@gmail.com>
2017-11-02 16:53:56 -0400

    autogenerate .XCompose

---
+++ mkcompose.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+import unicodedata
+
+keynames = dict([line.split() for line in """
+- minus
+~ asciitilde
+* asterisk
+. period
+= equal
+| bar
+> greater
+! exclam
+""".strip().splitlines()])
+
+def pairs(data):
+    return [line.split() for line in data.strip().splitlines()]
+
+# Math stuff
+macros = pairs("""
+-~       ⁓
+sq       √
+3sq      ∛
+4sq      ∜
+inf      ∞
+*x       ⨯
+*.       ⋅
+del      ∇
+part     ∂
+==       ≡
+int      ∫
+uint     ⨛
+lint     ⨜
+iint     ∬
+iiint    ∭
+iiiint   ⨌
+""")
+
+# Sets
+macros += pairs("""
+|C       ℂ
+|N       ℕ
+|P       ℙ
+|Q       ℚ
+|R       ℝ
+|Z       ℤ
+not      ¬
+AA       ∀
+EE       ∃
+land     ∧
+lor      ∨
+|-       ⊦
+|=       ⊧
+|>       ▷
+lin      ∈
+!lin     ∉
+""")
+
+# Greek letters
+macros += pairs("""
+alpha    α
+beta     β
+gamma    γ
+delta    δ
+epsilon  ε
+zeta     ζ
+eta      η
+theta    θ
+iota     ι
+kappa    κ
+lambda   λ
+mu       μ
+nu       ν
+xi       ξ
+omicron  ο
+pi       π
+rho      ρ
+sigma    σ
+tau      τ
+upsilon  υ
+phi      φ
+chi      χ
+psi      ψ
+omega    ω
+Alpha    Α
+Beta     Β
+Gamma    Γ
+Delta    Δ
+Epsilon  Ε
+Zeta     Ζ
+Eta      Η
+Theta    Θ
+Iota     Ι
+Kappa    Κ
+Lambda   Λ
+Mu       Μ
+Nu       Ν
+Xi       Ξ
+Omicron  Ο
+Pi       Π
+Rho      Ρ
+Sigma    Σ
+Tau      Τ
+Upsilon  Υ
+Phi      Φ
+Chi      Χ
+Psi      Ψ
+Omega    Ω
+""")
+
+for seq, char in macros:
+    seq_codes = ['Multi_key'] + [keynames.get(c, c) for c in seq]
+    xcmp_seq = ' '.join('<%s>' % s for s in seq_codes)
+    unicode_name = unicodedata.name(char)
+    print('%-40s: "%c" U%s  # %s' % (xcmp_seq, char, hex(ord(char))[2:].upper(), unicode_name))
