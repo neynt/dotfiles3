ea5e678f708f419e1a85988b7b06c8edc4d9782d
neynt <hyriodula@gmail.com>
2026-01-01 19:08:12 -0500

    _

--- .config/nvim/lua/plugins/baseline.lua
+++ .config/nvim/lua/plugins/baseline.lua
@@ -7,7 +7,7 @@ return {
       require('kanagawa').setup({
         transparent = true,
       })
-      vim.cmd.colorscheme('kanagawa-dragon')
+      vim.cmd.colorscheme('kanagawa-wave')
     end,
   },
   {
